093f143e2a307578b5e659c3206ad3525e162fd4
Thipok Cholsaipant <thipok.ch@gmail.com>
2022-07-21 22:10:41 +0700

    ci: com.github.ben-manes.versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.42.0") // Provides Dependencies Update Notice
     }
 }
 
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    id("com.github.ben-manes.versions") // Provides Dependencies Update Notice
 }
 
 kotlin {
